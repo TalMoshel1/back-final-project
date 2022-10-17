f263a1428ce99c12858c3ebd73a27cb4bc8a5838
Tal Moshel <talmoshel444@gmail.com>
2022-10-17 21:31:54 +0300

    feedFlowUpdate

--- src/controllers/feed-controller.ts
+++ src/controllers/feed-controller.ts
@@ -1,11 +1,23 @@
 import { AuthenticatedRequest } from '../types';
-import { serviceGetFollowersById } from '../services/feed-service';
+import { serviceGetFollowingsById } from '../services/feed-service';
+import { serviceGetPostsOfFollowings } from '../services/feed-service'
+import {PAGE_LIMIT} from './posts-controller'
 
+export async function getFeed(req: Request, res: Response) {
+    const followingsList = req.following
+    const followingAuthorsNameList = followingsList.map((follower) => {
+        return follower._id.toHexString()
+    })
+    const page =  0
+    const limit = 5
+    const offset = page * PAGE_LIMIT
+    const posts = await serviceGetPostsOfFollowings(followingAuthorsNameList)
+    res.send(posts)
+}
 
-export async function getFeed(req: AuthenticatedRequest, res: Response) {
+export async function FollowingsById(req: Request, res: Response, next: NextFunction) {
     const id = req.id.toHexString()
-    const feed = await serviceGetFollowersById(id)
-    return res.send(feed)
-
-
+    const following = await serviceGetFollowingsById(id)
+    req.following = following
+    next()
 }
